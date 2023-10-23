7f9ea3e7ffadb698e88002a9fe042a375274905a
obaidofj@gmail.com <obaidofj@gmail.com>
2023-10-23 08:45:48 +0300

    socket to be cont

--- socket.js
+++ socket.js
@@ -29,6 +29,6 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(3001, () => {
+  console.log('Server listening on port 3001');
 });
